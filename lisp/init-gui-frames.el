eff7c1cc7f07837c3242ee3e0f1c2e24f3d5291c
tangguangyue <aplgsdf@gmail.com>
2018-06-02 20:52:44 +0900

    hacking for gui

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -21,4 +21,41 @@
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 
+;; 显示当前行
+(global-hl-line-mode t)
+
+;; 关闭emacs的提示声音，移到第一行或者最后一行的时候
+(setq ring-bell-function 'ignore)
+
+;; 寻找匹配括号
+(defadvice show-paren-function (around fix-show-paren-function activate)
+   (cond ((looking-at-p "\\s(") ad-do-it)
+ 	(t (save-excursion
+ 	     (ignore-errors (backward-up-list))
+ 	     ad-do-it)))
+   )
+
+(add-hook 'emacs-list-mode-hook 'show-paren-mode)
+
+;; 删掉选中的模块
+(delete-selection-mode t)
+
+;; 代码缩进
+(defun indent-buffer()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-region-or-buffer()
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+	(progn
+	  (indent-region (region-beginning) (region-end))
+	  (message "Indent selected region."))
+      (progn
+	(indent-buffer)
+	(message "Indent buffer.")))))
+
+(global-set-key (kbd "<f5>") 'recentf-open-files)
+
 (provide 'init-gui-frames)
